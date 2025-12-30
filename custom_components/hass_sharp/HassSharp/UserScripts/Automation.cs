7b472d24e82df27793b58bcab74cf5add57153ab
Domagoj Zivanovic <domagoj.zivanovic@gmail.com>
2025-12-30 02:42:05 +0100

    Fixes types

--- custom_components/hass_sharp/HassSharp/UserScripts/Automation.cs
+++ custom_components/hass_sharp/HassSharp/UserScripts/Automation.cs
@@ -34,7 +34,7 @@ public abstract class Automation : UserScriptClassBase
         return state;
     }
 
-    protected EntityRef<T> Entity<T>(T entityWrapper, [CallerMemberName] string? caller = null)
+    protected EntityRef<T> Entity<T>(EntityRefWrapper<T> entityWrapper, [CallerMemberName] string? caller = null)
         where T : EntityRefWrapper<T>
     {
         var raw = GetEntityValueTracked(entityWrapper.EntityId, caller!);
@@ -69,7 +69,7 @@ public abstract class Automation : UserScriptClassBase
         };
     }
 
-    protected EntityRef<T> EntityUntracked<T>(T entityRefWrapper) where T : EntityRefWrapper<T>
+    protected EntityRef<T> EntityUntracked<T>(EntityRefWrapper<T> entityRefWrapper) where T : EntityRefWrapper<T>
     {
         var raw = GetEntityValueUntracked(entityRefWrapper.EntityId);
         if (raw == null) throw new($"Entity with id {entityRefWrapper.EntityId} not found");
