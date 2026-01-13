2051f66e91112bb23eb5739f5becc2cfef350f4a
Domagoj Zivanovic <domagoj.zivanovic@gmail.com>
2026-01-14 00:24:55 +0100

    Fixes automation list view

--- custom_components/hass_sharp/frontend/src/views/AutomationsListView.tsx
+++ custom_components/hass_sharp/frontend/src/views/AutomationsListView.tsx
@@ -1,6 +1,6 @@
 import type { RouteSectionProps } from '@solidjs/router'
 import { HassCtx } from '../context'
-import { useContext } from 'solid-js'
+import { useContext, For } from 'solid-js'
 import type { UserScriptDTO } from '../types'
 import { useQuery } from '@tanstack/solid-query'
 
@@ -14,33 +14,35 @@ export const AutomationsListView = (_props: RouteSectionProps) => {
 
   return (
     <div class="w-full overflow-y-auto p-4">
-      {isPending && <span>Loading...</span>} {/* TODO: Spinner */}
+      {isPending && <span>Loading...</span>}
       <ha-md-list class="w-full p-0! rounded-md">
-        {userFiles?.map(file => (
-          <>
-            <ha-md-list-item>
-              <div slot="headline" class="font-bold text-blue-400">
-                {file.fileName}
+        <For each={userFiles}>
+          {file => (
+            <>
+              <ha-md-list-item>
+                <div slot="headline" class="font-bold text-blue-400">
+                  {file.fileName}
+                </div>
+                <ha-icon slot="start" icon="mdi:file-code-outline" class="text-blue-400"></ha-icon>
+              </ha-md-list-item>
+              <div class="flex flex-col">
+                {file.classes.map((klass, idx) => (
+                  <ha-md-list-item type="button" href={`/hass-sharp/automations/${file.fileName}`}>
+                    <div slot="headline">{klass.name.split('.').pop()}</div>
+                    <div slot="supporting-text" class="text-xs text-gray-400">
+                      {klass.methods.length} methods
+                    </div>
+                    <ha-icon slot="start" icon="mdi:code-braces" class="ml-4 opacity-70"></ha-icon>
+                    <ha-icon-button slot="end">
+                      <ha-icon icon="mdi:chevron-right"></ha-icon>
+                    </ha-icon-button>
+                    {idx < file.classes.length - 1 && <div slot="bottom" class="border-b border-gray-800 ml-16"></div>}
+                  </ha-md-list-item>
+                ))}
               </div>
-              <ha-icon slot="start" icon="mdi:file-code-outline" class="text-blue-400"></ha-icon>
-            </ha-md-list-item>
-            <div class="flex flex-col">
-              {file.classes.map((klass, idx) => (
-                <ha-md-list-item type="button" href={`/hass-sharp/automations/${file.fileName}`}>
-                  <div slot="headline">{klass.name.split('.').pop()}</div>
-                  <div slot="supporting-text" class="text-xs text-gray-400">
-                    {klass.methods.length} methods
-                  </div>
-                  <ha-icon slot="start" icon="mdi:code-braces" class="ml-4 opacity-70"></ha-icon>
-                  <ha-icon-button slot="end">
-                    <ha-icon icon="mdi:chevron-right"></ha-icon>
-                  </ha-icon-button>
-                  {idx < file.classes.length - 1 && <div slot="bottom" class="border-b border-gray-800 ml-16"></div>}
-                </ha-md-list-item>
-              ))}
-            </div>
-          </>
-        ))}
+            </>
+          )}
+        </For>
       </ha-md-list>
     </div>
   )
